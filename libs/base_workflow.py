d406fb57f67d93198ecab84c4f5f201f7b6a230e
6-6ho <junho.song@deepingsource.io>
2025-08-28 16:09:32 +0900

    GPT-4o로 복원 - 가장 빠른 성능 (GPT-5-mini 대비 10배 빨라짐)

--- libs/base_workflow.py
+++ libs/base_workflow.py
@@ -162,7 +162,7 @@ class BaseWorkflow(ABC, Generic[StateType]):
             if llm_model is None:
                 # 기본 모델 사용
                 from langchain_openai import ChatOpenAI
-                llm_model = ChatOpenAI(model="gpt-5-mini")
+                llm_model = ChatOpenAI(model="gpt-4o", temperature=0.1)
             
             # 프롬프트 생성
             prompt = prompt_template.format(data=data)
